92fc781ffe8138c35177a25a472a625a6471f953
huangchao <huangc@figo.cn>
2018-05-25 15:25:58 +0800

    A 服务消费者（rest+ribbon）& 断路器（Hystrix）

---
+++ service-ribbon/src/main/java/cn/figo/cloud/ServiceRibbonApplication.java
@@ -0,0 +1,34 @@
+package cn.figo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 在工程的启动类中,通过@EnableDiscoveryClient向服务中心注册；
+ *
+ * 在程序的启动类中 加 @EnableHystrix注解开启Hystrix：断路器（Hystrix）
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+
+	/**
+	 * 向程序的ioc注入一个bean: restTemplate;
+	 * 并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能。
+	 */
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ service-ribbon/src/main/java/cn/figo/cloud/web/HelloController.java
@@ -0,0 +1,25 @@
+package cn.figo.cloud.web;
+
+import cn.figo.cloud.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: HuangC
+ * @Description: 在controller中用调用 HelloService 的方法
+ * @Date: 2018/5/23 16:53
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+}
