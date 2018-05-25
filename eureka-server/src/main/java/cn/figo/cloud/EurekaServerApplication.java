3e9ac9b5dbfde3a76180bfb76d9a5395158fa977
huangchao <huangc@figo.cn>
2018-05-25 15:23:04 +0800

    A 服务注册中心(Eureka)

---
+++ eureka-server/src/main/java/cn/figo/cloud/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package cn.figo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动一个服务注册中心，添加 注解@EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
